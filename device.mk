3aa521248b9852eaf9e9d922c43e7a466a6540c9
Allan DSouza <allan@allan.co.in>
2024-05-01 22:58:30 +0300

    sky: Derp it

--- device.mk
+++ device.mk
@@ -356,7 +356,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay-derp
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
